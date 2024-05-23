f8e5acd6960436bc5acfed9a1fb3f95083c8881b
Yusuf  Eren Hoşgör <119801795+wicklash@users.noreply.github.com>
2024-05-23 20:04:36 +0300

    Add files via upload

---
+++ FormReminder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class FormReminder : Form
+    {
+        private User currentUser;
+        private UserManager userManager;
+        public FormReminder(User currentUser,UserManager userManager)
+        {
+            InitializeComponent();
+            this.currentUser = currentUser;
+            this.userManager = userManager;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void btnAlt_Click(object sender, EventArgs e)
+        {
+            this.WindowState= FormWindowState.Minimized;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
